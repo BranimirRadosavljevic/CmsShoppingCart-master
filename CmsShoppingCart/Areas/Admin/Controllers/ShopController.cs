6415d4f92e680756d07a20b12ff442d76ab117dc
Branimir Radosavljevic <rbranimir@ymail.com>
2020-01-05 18:03:25 +0100

    admin shop 2

---
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,29 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop
+        public ActionResult Categories()
+        {
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories
+                                .ToArray()
+                                .OrderBy(x => x.Sorting)
+                                .Select(x => new CategoryVM(x))
+                                .ToList(); 
+            }
+            return View(categoryVMList);
+        }
+    }
+}
