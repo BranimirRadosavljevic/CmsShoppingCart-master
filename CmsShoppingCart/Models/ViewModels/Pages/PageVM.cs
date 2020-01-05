44d1a422955cafb34f2e1bd44752e85de8aebdf8
Branimir Radosavljevic <rbranimir@ymail.com>
2020-01-05 14:51:18 +0100

    admin pages 11 fileman

--- CmsShoppingCart/Models/ViewModels/Pages/PageVM.cs
+++ CmsShoppingCart/Models/ViewModels/Pages/PageVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CmsShoppingCart.Models.ViewModels.Pages
 {
@@ -16,6 +17,7 @@ namespace CmsShoppingCart.Models.ViewModels.Pages
         public string Slug { get; set; }
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 3)]
+        [AllowHtml]
         public string Body { get; set; }
         public int Sorting { get; set; }
         public bool HasSidebar { get; set; }
--- CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
+++ CmsShoppingCart/Models/ViewModels/Pages/SidebarVM.cs
@@ -3,12 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CmsShoppingCart.Models.ViewModels.Pages
 {
     public class SidebarVM
     {
         public int Id { get; set; }
+
+        [AllowHtml]                            
         public string Body { get; set; }
 
         public SidebarVM()
