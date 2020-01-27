d7534e44a8d8abdf1d508d6b515e873fdbcc8a01
Bane Radosavljević <r_bane@hotmail.com>
2020-01-27 17:45:58 +0100

    Admin shop 12

--- CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -2,8 +2,10 @@
 using CmsShoppingCart.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
@@ -153,7 +155,7 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
                 product.CategoryId = model.CategoryId;
 
                 CategoryDTO catDTO = db.Categories.FirstOrDefault(m => m.Id == model.CategoryId);
-                model.CategoryName = catDTO.Name;
+                product.CategoryName = catDTO.Name;
 
                 db.Products.Add(product);
                 db.SaveChanges();
@@ -165,9 +167,78 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
 
             #region Upload Image
 
+            var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+            var pathString1 = Path.Combine(originalDirectory.ToString(), "Products");
+            var pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+            var pathString3 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Thumbs");
+            var pathString4 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+            var pathString5 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+            if (!Directory.Exists(pathString1))
+            {
+                Directory.CreateDirectory(pathString1);
+            }
+
+            if (!Directory.Exists(pathString2))
+            {
+                Directory.CreateDirectory(pathString2);
+            }
+
+            if (!Directory.Exists(pathString3))
+            {
+                Directory.CreateDirectory(pathString3);
+            }
+
+            if (!Directory.Exists(pathString4))
+            {
+                Directory.CreateDirectory(pathString4);
+            }
+
+            if (!Directory.Exists(pathString5))
+            {
+                Directory.CreateDirectory(pathString5);
+            }
+
+            if (file != null && file.ContentLength > 0)
+            {
+                string ext = file.ContentType.ToLower();
+                if (ext != "image/jpg" &&
+                    ext != "image/jpeg" &&
+                    ext != "image/pjpeg" &&
+                    ext != "image/gif" &&
+                    ext != "image/x-png" &&
+                    ext != "image/png")
+                {
+                    using (Db db = new Db())
+                    {
+                        model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                        ModelState.AddModelError("", "The image was not uploaded - wrong image extension.");
+                        return View(model);
+                    }
+                }
+
+                string imageName = file.FileName;
+
+                using (Db db = new Db())
+                {
+                    ProductDTO dto = db.Products.Find(id);
+                    dto.ImageName = imageName;
+                    db.SaveChanges();
+                }
+
+                var path = string.Format("{0}\\{1}", pathString2, imageName);
+                var path2 = string.Format("{0}\\{1}", pathString3, imageName);
+
+                file.SaveAs(path);
+
+                WebImage img = new WebImage(file.InputStream);
+                img.Resize(200, 200);
+                img.Save(path2);
+            }
+
             #endregion
 
-            return RedirectToAction("Products");
+            return RedirectToAction("AddProduct");
         }
     }
 }
