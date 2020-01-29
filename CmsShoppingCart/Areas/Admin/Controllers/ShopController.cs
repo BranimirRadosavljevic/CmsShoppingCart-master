7cc268fe4e200e93eb5535ad5d5d7d5e3a1238fa
Branimir Radosavljevic <rbranimir81@gmail.com>
2020-01-29 13:04:35 +0100

    Admin shop 18

--- CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -391,5 +391,26 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
                 #endregion
                 return RedirectToAction("EditProduct");
         }
+ 
+        public ActionResult DeleteProduct(int id)
+        {
+            using (Db db = new Db())
+            {
+                ProductDTO dto = db.Products.Find(id);
+                db.Products.Remove(dto);
+                db.SaveChanges();
+            }
+
+            var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+            string pathString = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+
+            if (Directory.Exists(pathString))
+            {
+                Directory.Delete(pathString, true);
+            }
+
+
+            return RedirectToAction("Products");
+        }
     }
 }
