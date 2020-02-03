74ab2f3475437e5613978ad82f5ef7224901883d
Branimir Radosavljevic <rbranimir81@gmail.com>
2020-02-03 13:49:32 +0100

    shop front 3

--- CmsShoppingCart/Controllers/ShopController.cs
+++ CmsShoppingCart/Controllers/ShopController.cs
@@ -29,5 +29,31 @@ namespace CmsShoppingCart.Controllers
 
             return PartialView(categoryVMList);
         }
+
+        public ActionResult Category(string name)
+        {
+            List<ProductVM> productVMList;
+
+            using (Db db = new Db())
+            {
+                CategoryDTO categoryDTO = db.Categories.Where(m => m.Slug == name).FirstOrDefault();
+                int catId = categoryDTO.Id;
+
+                productVMList = db.Products
+                                    .ToArray()
+                                    .Where(m => m.CategoryId == catId)
+                                    .Select(m => new ProductVM(m))
+                                    .ToList();
+
+                //var productCat = db.Products.Where(m => m.CategoryId == catId).FirstOrDefault();
+                //ViewBag.CategoryName = productCat.CategoryName;
+
+                var productCat = db.Categories.Find(catId);
+                ViewBag.CategoryName = productCat.Name;
+            }
+
+            return View(productVMList);
+        }
+
     }
 }
