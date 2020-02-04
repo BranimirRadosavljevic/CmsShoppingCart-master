2dfa9dd485708373f3f0bcfbc0191e7a7824e59d
Branimir Radosavljevic <rbranimir81@gmail.com>
2020-02-04 09:54:15 +0100

    car front 1

---
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using CmsShoppingCart.Models.ViewModels.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CartPartial()
+        {
+            CartVM model = new CartVM();
+            int qty = 0;
+            decimal price = 0m;
+
+            if (Session["cart"] != null)
+            {
+                var list = (List<CartVM>)Session["cart"];
+                foreach (var item in list)
+                {
+                    qty += item.Quantity;
+                    price += item.Total;
+                }
+            }
+            else
+            {
+                model.Quantity = 0;
+                model.Price = 0m;
+            }
+
+            return PartialView(model);
+        }
+    }
+}
