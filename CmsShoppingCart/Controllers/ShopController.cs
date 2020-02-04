b58eace1a50616704627352e01a21258c15d7518
Branimir Radosavljevic <rbranimir81@gmail.com>
2020-02-04 09:08:30 +0100

    shop front 3

--- CmsShoppingCart/Controllers/ShopController.cs
+++ CmsShoppingCart/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using CmsShoppingCart.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -53,6 +54,33 @@ namespace CmsShoppingCart.Controllers
             }
 
             return View(productVMList);
+        }
+
+        [ActionName("product-details")]
+        public ActionResult ProductDetails(string name)
+        {
+            ProductVM model;
+            ProductDTO dto;
+
+            int id = 0;
+
+            using (Db db = new Db())
+            {
+                if (!db.Products.Any(m => m.Slug == name))
+                {
+                    return RedirectToAction("Index");
+                }
+
+                dto = db.Products.Where(m => m.Slug == name).FirstOrDefault();
+                id = dto.Id;
+
+                model = new ProductVM(dto);
+            }
+
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs"))
+                                                .Select(m => Path.GetFileName(m));
+
+            return View("ProductDetails", model);
         }
 
     }
