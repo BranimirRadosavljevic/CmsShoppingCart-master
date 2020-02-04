304d14d300a1ec73ba058014035c5c4bd15311a3
Bane Radosavljević <r_bane@hotmail.com>
2020-02-04 21:01:13 +0100

    cart front finished

--- CmsShoppingCart/Controllers/CartController.cs
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -79,7 +79,7 @@ namespace CmsShoppingCart.Controllers
                         Quantity = 1,
                         Price = product.Price,
                         Image = product.ImageName
-                    }) ;
+                    });
                 }
                 else
                 {
@@ -116,5 +116,42 @@ namespace CmsShoppingCart.Controllers
                 return Json(result, JsonRequestBehavior.AllowGet);
             }
         }
+
+        public JsonResult DecrementProduct(int productId)
+        {
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                CartVM model = cart.FirstOrDefault(m => m.ProductId == productId);
+
+                if (model.Quantity > 1)
+                {
+                    model.Quantity--;
+                }
+                else
+                {
+                    model.Quantity = 0;
+                    cart.Remove(model);
+                }
+
+                var result = new { qty = model.Quantity, price = model.Price };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public void RemoveProduct(int productId)
+        {
+            List<CartVM> cart = Session["cart"] as List<CartVM>;
+
+            using (Db db = new Db())
+            {
+                CartVM model = cart.FirstOrDefault(m => m.ProductId == productId);
+
+                cart.Remove(model);                               
+                
+            }
+        }
+
     }
 }
