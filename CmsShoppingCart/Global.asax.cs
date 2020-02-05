67e109e313b8e2e55b81e93bbd63e48816160b9f
Bane Radosavljević <r_bane@hotmail.com>
2020-02-05 20:37:38 +0100

    roles

--- CmsShoppingCart/Global.asax.cs
+++ CmsShoppingCart/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CmsShoppingCart.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,29 @@ namespace CmsShoppingCart
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            if (User == null)
+            {
+                return;
+            }
+
+            string username = Context.User.Identity.Name;
+
+            string[] roles = null;
+
+            using (Db db = new Db())
+            {
+                UserDTO dto = db.Users.FirstOrDefault(m => m.Username == username);
+                roles = db.UserRoles.Where(m => m.UserId == dto.Id).Select(m => m.Role.Name).ToArray();
+            }
+
+            IIdentity userIdentity = new GenericIdentity(username);
+            IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+            Context.User = newUserObj;
         }
     }
 }
